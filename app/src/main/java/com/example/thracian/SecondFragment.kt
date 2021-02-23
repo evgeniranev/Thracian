64f28333bfd252be36799e12c5834b79125df5dc
Evgeny Ranev <licekniga@abv.bg>
2021-02-23 18:39:30 +0200

    Implemented list of subcategories

--- app/src/main/java/com/example/thracian/SecondFragment.kt
+++ app/src/main/java/com/example/thracian/SecondFragment.kt
@@ -11,21 +11,21 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
-
+class SecondFragment : Fragment(R.layout.fragment_second) {
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
+
